951519b314fbec134b284cbb36d9652dcdad6572
Víctor Hernández <victor.hernandez@qacg.com>
2019-10-25 10:36:46 -0500

    Add flavors FREE & PAID

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,48 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import javax.annotation.meta.When;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        switch (BuildConfig.KEY_FLAVOR){
+            case "KEY_FREE":
+                break;
+            case "KEY_PAID":
+                break;
+        }
+
+        AdView mAdView = root.findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        return root;
+
+    }
+
+
+}
